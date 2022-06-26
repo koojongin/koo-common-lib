acf43fdbe2b1a334e0260e652979827f2907e83e
jonginkoo <jiku90@ddingdong.net>
2022-06-27 00:16:20 +0900

    'chore:test'

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,8 @@
 import {CardOption as _CardOption} from "./enums/card.enum";
 import {ICard as _ICard} from "./interfaces/ICard";
 
+export = Koo;
+export as namespace Koo;
 declare namespace Koo {
     export type CardOption = _CardOption;
 
